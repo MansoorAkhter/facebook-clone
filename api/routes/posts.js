f067911813bc2fd3bfb2fec3ad39a011f38b4000
Mansoor Akhter <mansoor21st@Gmail.com>
2023-05-08 17:23:13 +0500

    get post api done

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -69,6 +69,18 @@ router.post("/:id/like", async (req, res) => {
     }
 
 })
+
+
+
+
 // Get Post
+router.get("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
 // Get timeline Post
 module.exports = router
