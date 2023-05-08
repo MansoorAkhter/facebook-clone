9c61fca0ebc31383ffa71a861b05774f2ac386a9
Mansoor Akhter <mansoor21st@Gmail.com>
2023-05-08 17:03:30 +0500

    like & dislike post api's, done

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -49,7 +49,26 @@ router.delete("/:id", async (req, res) => {
         res.status(500).json(error)
     }
 })
-// Like Post
+
+
+
+
+// Like & dislike Post
+router.post("/:id/like", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post.likes.includes(req.body.userId)) {
+            await post.updateOne({ $push: { likes: req.body.userId } });
+            res.status(200).json("Post has been liked")
+        } else {
+            await post.updateOne({ $pull: { likes: req.body.userId } });
+            res.status(200).json("Post has been disliked")
+        }
+    } catch (error) {
+        res.status(500).json(error)
+    }
+
+})
 // Get Post
 // Get timeline Post
 module.exports = router
