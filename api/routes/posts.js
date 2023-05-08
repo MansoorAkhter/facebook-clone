bb2c7ac429936de5f142c5438fbffe8a639b135f
Mansoor Akhter <mansoor21st@Gmail.com>
2023-05-08 19:33:39 +0500

    timeline Posts api pending

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -82,5 +82,27 @@ router.get("/:id", async (req, res) => {
         res.status(500).json(error)
     }
 })
+
+
+
+
 // Get timeline Post
+router.get("/timeline/all", async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.body.userId);
+        const userPosts = await Post.find({ userId: currentUser._id });
+
+        const friendPosts = await Promise.all(
+            currentUser.followings.map((friendId) => {
+                return Post.find({ userId: friendId });
+            })
+        );
+        res.json(userPosts.concat(...friendPosts))
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+
+
 module.exports = router
