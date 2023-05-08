23d022b36730a723a13d959664ca21d5f88f463a
Mansoor Akhter <mansoor21st@Gmail.com>
2023-05-08 16:40:07 +0500

    update & delete post api's, done

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -13,8 +13,42 @@ router.post("/", async (req, res) => {
         res.status(500).json(err)
     }
 })
+
+
+
+
 // Update Post
+router.put("/:id", async (req, res) => {
+    const post = await Post.findById(req.params.id);
+    try {
+        if (post.userId === req.body.userId) {
+            await post.updateOne({ $set: req.body });
+            res.status(200).json("the post has been updated")
+        } else {
+            res.status(403).json("you can update only your post")
+        }
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+
+
+
 // Delete Post
+router.delete("/:id", async (req, res) => {
+    const post = await Post.findById(req.params.id);
+    try {
+        if (post.userId === req.body.userId) {
+            await post.deleteOne();
+            res.status(200).json("the post has been deleted");
+        } else {
+            res.status(403).json("you can delete only your post")
+        }
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
 // Like Post
 // Get Post
 // Get timeline Post
