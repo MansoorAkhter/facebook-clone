16c1d87a1d95b285c99c08b942f89d64669522cd
Mansoor Akhter <mansoor21st@Gmail.com>
2023-05-09 18:17:43 +0500

    getMessage & deleteConversation api's done

--- api/routes/conversation.js
+++ api/routes/conversation.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 const Conversation = require("../models/Conversation");
+const User = require("../models/User");
 
 // new conversation
 router.post("/", async (req, res) => {
-
     const newConversation = new Conversation({
         members: [req.body.senderId, req.body.receiverId]
     });
@@ -25,11 +25,41 @@ router.post("/", async (req, res) => {
     try {
         const conversation = await Conversation.find(req.body.id);
         members: { $in: [req.params.userId] };
-
         res.status(200).json(conversation);
-    } catch (error) {
+    }
+    catch (error) {
         res.status(500).json(error)
     }
 })
 
-module.exports = router
+
+
+
+// delete conversation
+router.delete("/:conversationId", async (req, res) => {
+    const conversation = await Conversation.findById(req.params.conversationId);
+    const member = await Conversation.find(req.body.members);
+    const findUser = await User.findById(req.body.userId);
+    const authUser = member[0].members.find(i => findUser._id.equals(i));
+
+    try {
+        if (authUser) {
+            await conversation.deleteOne();
+            res.status(200).json("the conversation has been deleted");
+        } else {
+            res.status(403).json("you can delete only your conversation")
+        }
+    }
+    catch (error) {
+        res.status(500).json(error)
+    }
+});
+
+
+
+module.exports = router;
+
+
+
+
+
--- api/routes/message.js
+++ api/routes/message.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const Message = require("../models/Message");
 
+
+
 // send message
 router.post("/", async (req, res) => {
     const newMessage = new Message(req.body);
@@ -20,9 +22,15 @@ router.post("/", async (req, res) => {
 // get message
 router.get("/:conversationId", async (req, res) => {
     try {
-        const messages = await Message.find({ conversationId: req.params.conversationId })
+        const messages = await Message.find({ conversationId: req.params.conversationId });
+        res.status(200).json(messages)
     } catch (error) {
         res.status(500).json(error)
     }
 })
-module.exports = router
+module.exports = router;
+
+
+
+
+// delete message
